ccf7edc4c7370d246f8d103672b66bfd149bb98d
João Henrique <joaohen.t8@gmail.com>
2025-03-11 08:55:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function addTech() {
+    const contactSection = document.getElementById('contact-list')
+    
+
+
+    const nameInput = document.createElement('input')
+    nameInput.type = 'text'
+    nameInput.name = 'fullname'
+
+    contactSection.appendChild(nameInput);
+
+
+}
